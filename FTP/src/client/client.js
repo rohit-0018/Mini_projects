4c031b7b7e1ff71b182d18a5ca84423baa893387
Rohit pandey <rohitpandey0018@gmail.com>
2021-05-23 11:15:11 +0530

    Boilerplate code

---
+++ FTP/src/client/client.js
@@ -0,0 +1,8 @@
+const net = require('net');
+const port = 7070;
+const host = '127.0.0.1';
+
+const server = net.createServer();
+server.listen(port, host, () => {
+    console.log('TCP Server is running on port ' + port +'.');
+});
