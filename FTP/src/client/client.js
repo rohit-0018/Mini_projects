7346bfb02fee0edbf56ecb1ba40747632203743c
Rohit pandey <rohitpandey0018@gmail.com>
2021-05-23 14:57:09 +0530

    cleanup

--- FTP/src/client/client.js
+++ FTP/src/client/client.js
@@ -1,8 +1,15 @@
 const net = require('net');
+const client = new net.Socket();
 const port = 7070;
 const host = '127.0.0.1';
 
-const server = net.createServer();
-server.listen(port, host, () => {
-    console.log('TCP Server is running on port ' + port +'.');
+client.connect(port, host, function() {
+    console.log('Connected');
+    client.write("Hello From Client " + client.address().address);
+    client.on('data', function(data) {
+        console.log('Server Says : ' + data);
+    });
+    client.on('close', function() {
+        console.log('Connection closed');
+    });
 });
